8502a236ed10cef32c9884a80d6cc9c1ac7e2e32
Breno Leitao <leitao@debian.org>
2023-10-31 09:50:11 -0700

    add summary

--- maps/parse_procmaps.py
+++ maps/parse_procmaps.py
@@ -13,13 +13,12 @@ def find_pid(name: str) -> int:
     proc = subprocess.Popen(command, stdout=subprocess.PIPE)
     for line_ in proc.stdout:
         line = line_.decode().strip()
-        print(line)
         if name in line and "python" not in line:
-            print(f"Found process:\n   *  {line}", file=sys.stderr)
+            # print(f"Found process:\n   *  {line}", file=sys.stderr)
             ret = line.split()
             return ret[1]
 
-def mem_size(line: str):
+def mem_size(line: str, verbose: bool):
     memrange= line.split(" ")[0]
     area = line.split(" ")[-1].strip()
     start, end = memrange.split("-")
@@ -30,30 +29,36 @@ def mem_size(line: str):
     global entries
     entries += 1
 
-    print(f"{size :<10} : {area}")
+    if verbose:
+        print(f"{size :<10} : {area}")
 
 
-def read_proc_maps(pid: int):
+def read_proc_maps(pid: int, verbose: bool):
     with open(f"/proc/{pid}/maps") as f:
         for line in f.readlines():
-            mem_size(line)
+            mem_size(line, verbose)
 
-def main(process: str) -> None:
+def main(process: str, summary: bool) -> None:
     pid = find_pid(process)
     if not pid:
         print("Pid not found")
         return
 
-    print(f"Pid id {pid}", file=sys.stderr)
-    read_proc_maps(pid)
-    global total_size
-    global entries
-    print(f"Size = {total_size/(1024*1024*1024)} and entries = {entries}", file=sys.stderr)
+    # print(f"Pid id {pid}", file=sys.stderr)
+    read_proc_maps(pid, not summary)
+
+    # Print summary
+    if summary:
+        global total_size
+        global entries
+        print(f"Size = {total_size/(1024*1024*1024)} and entries = {entries}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='parse /proc/pid/maps for a process name')
     parser.add_argument('name', help='process name')
+    parser.add_argument('--sum', action='store_true', help='print summary')
     args = parser.parse_args()
     process_name = args.name
-    main(process_name)
+
+    main(process_name, args.sum)
