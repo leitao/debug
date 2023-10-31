558295387728a7bb231f71e66aa8f2dcf4acc9e7
Breno Leitao <leitao@debian.org>
2023-10-31 06:46:01 -0700

    Dump lines

--- maps/parse_procmaps.py
+++ maps/parse_procmaps.py
@@ -1,23 +1,35 @@
-#!env python3.11
+#!/usr/bin/python3
 
 import subprocess
 import argparse
+import sys
+
+total_size = 0
+entries = 0
 
 def find_pid(name: str) -> int:
-    command = ["ps", "a"]
+    command = ["ps", "aux"]
 
-
-    process = subprocess.Popen(command, stdout=subprocess.PIPE)
-    while True:
-        line = process.stdout.readline().decode()
-        if name in line:
-            return line.split(" ")[0]
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+    for line_ in proc.stdout:
+        line = line_.decode().strip()
+        print(line)
+        if name in line and "python" not in line:
+            print(f"Found process:\n   *  {line}", file=sys.stderr)
+            ret = line.split()
+            return ret[1]
 
 def mem_size(line: str):
     memrange= line.split(" ")[0]
     area = line.split(" ")[-1].strip()
     start, end = memrange.split("-")
     size = int(f"0x{end}", 16) - int(f"0x{start}", 16)
+    # Global variables
+    global total_size
+    total_size += size
+    global entries
+    entries += 1
+
     print(f"{size :<10} : {area}")
 
 
@@ -28,8 +40,15 @@ def read_proc_maps(pid: int):
 
 def main(process: str) -> None:
     pid = find_pid(process)
-    print(f"Pid id {pid}")
+    if not pid:
+        print("Pid not found")
+        return
+
+    print(f"Pid id {pid}", file=sys.stderr)
     read_proc_maps(pid)
+    global total_size
+    global entries
+    print(f"Size = {total_size/(1024*1024*1024)} and entries = {entries}", file=sys.stderr)
 
 
 if __name__ == "__main__":
