eca0b4b112aa1f0a5f4076a29c3f12e51bad13b2
Breno Leitao <leitao@debian.org>
2023-11-01 10:29:54 -0700

    Dynamic memory allocation

--- examples/thp_alloc.c
+++ examples/thp_alloc.c
@@ -12,40 +12,50 @@ static void write_to_mem(char *mem, size_t size)
 	}
 }
 
-int main() {
-    size_t size = 20 * 1024 * 1024;  // 20 MB
-    char *memory[MAX];
+int main(int argc, char** argv) {
+	char *memory[MAX];
+	size_t size;
+
+	if (argc != 2) {
+		size = 20;
+	} else {
+		size = atoi(argv[1]);
+	}
+	printf("Using a total of %lu MB\n", size);
+	fflush(stdout); // Flush the stdout buffer
 
 
-   for (int i = 0 ; i < MAX; i++) { 
-	    memory[i] = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	size *= 1024 * 1024; 
 
-	    if (memory == MAP_FAILED) {
-		perror("mmap");
-		return 1;
-	    }
-   }
 
-   while (true) {
-	for (int i = 0 ; i < MAX; i++) { 
-		if (madvise(memory[i], size, MADV_HUGEPAGE) != 0) {
-			perror("madvise");
-			return 1;
+
+	while (true) {
+		for (int i = 0 ; i < MAX; i++) { 
+			memory[i] = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+			if (memory == MAP_FAILED) {
+				perror("mmap");
+				return 1;
+			}
 		}
 
-		write_to_mem(memory[i], size);
+		for (int i = 0 ; i < MAX; i++) { 
+			if (madvise(memory[i], size, MADV_HUGEPAGE) != 0) {
+				perror("madvise");
+				return 1;
+			}
+
+			write_to_mem(memory[i], size);
+		}
+
+		for (int i = 0 ; i < MAX; i++) { 
+			int ret = munmap(memory[i],  size);
+			if (ret) {
+				perror("munmap");
+				return 1;
+			}
+		}
 	}
-   }
 
-   for (int i = 0 ; i < MAX; i++) { 
-	    /* printf("Allocated memory with MADV_HUGEPAGE hint\n"); */
-	    int ret = munmap(memory[i],  size);
-	    if (ret) {
-		perror("munmap");
-		return 1;
-	    }
-    }
-
-
-    return 0;
+	return 0;
 }
