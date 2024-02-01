f30f5099fcdb9858f323b506dd7a566d3bf51a85
Breno Leitao <leitao@debian.org>
2024-02-01 07:31:48 -0800

    Parse both msrs

--- examples/x2apic/read_msr.py
+++ examples/x2apic/read_msr.py
@@ -15,9 +15,26 @@ if stderr:
 
 
 val =  int(stdout.decode().strip(), 16)
+
 x2apic = bool(val & (1 << 10))
-apic = bool(val & (1 << 10))
+xapic = bool(val & (1 << 11))
 
-print(f"X2APIC enabled: {x2apic}")
-print(f"APIC enabled: {apic}")
 
+ # From table 2.1 
+ # https://courses.cs.washington.edu/courses/cse451/24wi/documentation/x2apic.pdf
+ # xAPIC | x2APIC    | Msg
+ # 0     |   0       | local APIC is disabled
+ # 0     |   1       | Invalid
+ # 1     |   0       | local APIC is enabled in xAPIC mode
+ # 1     |   1       | local APIC is enabled in x2APIC mode
+
+if xapic and x2apic:
+    msg = "local APIC is enabled in x2APIC mode"
+elif xapic and not x2apic:
+    msg =  "local APIC is enabled in xAPIC mode"
+elif not xapic and x2apic:
+    msg = "Invalid"
+else:
+    msg = "local APIC is disabled"
+
+print(msg)
