0315dda65d6c35020c0b25781af7e6de84b9d0f5
Breno Leitao <leitao@debian.org>
2024-05-21 07:45:50 -0700

    Call sync

--- discard/discard.c
+++ discard/discard.c
@@ -7,28 +7,31 @@
 #include <string.h> // strdup
 
 
-#define THREADS 100
 #define MAXPATH 1024
+#define THREADS 1
+#define SYNC_AFTER_FILES 1000/THREADS
 
 char *dstdir = "/tmp";
 
 void *do_it(void *ptr)
 {
-	char filename[MAXPATH];
 	const int buffer[] = {1,2,3,4,5,6,7,8,9};
+	int sz = 4096 / sizeof(buffer) + 1;
+	char filename[MAXPATH];
 	int fd;
 
 	snprintf(filename, MAXPATH, "%s/tmp_XXXXXX", dstdir);
 
 	fd = mkstemp(filename);
-
 	if (fd < 0){
 		perror("Failed to open tmp file");
 		exit(1);
 	}
 
 	/* printf("filename=%s sizeof=%d\n", filename, sizeof(buffer)); */
-	write(fd, &buffer, sizeof(buffer));
+	while (sz--)
+		write(fd, &buffer, sizeof(buffer));
+
 	close(fd);
 	unlink(filename);
 
@@ -41,7 +44,7 @@ int main (int argc, char **argv)
 	pthread_t threads[THREADS];
 	int ret[THREADS];
 	bool loop;
-	int i, c;
+	int i, c, counter = 0;
 
 	while ((c = getopt(argc, argv, "l-d:")) != -1) {
 		switch (c) {
@@ -62,6 +65,11 @@ int main (int argc, char **argv)
 
 		for (i = 0 ; i < THREADS; i++) {
 			pthread_join(threads[i], NULL);
+		}
+
+		if (counter++ % SYNC_AFTER_FILES == 0) {
+			printf("Issuing a sync()\n");
+			sync();
 		}
 	} while (loop);
 
