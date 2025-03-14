1f47351dc402c09f45a7c90269427e7d599f12c0
Breno Leitao <leitao@debian.org>
2025-03-14 10:22:06 -0700

    decode

--- page_Fault/decode.py
+++ page_Fault/decode.py
@@ -1,24 +1,26 @@
 #!env python3
-  ##define VM_NONE         0x00000000
-  ##define VM_READ         0x00000001      /* currently active flags */
-  ##define VM_WRITE        0x00000002
-  ##define VM_EXEC         0x00000004
-  ##define VM_SHARED       0x00000008
-  #/* mprotect() hardcodes VM_MAYREAD >> 4 == VM_READ, and so for r/w/x bits. */
-  ##define VM_MAYREAD      0x00000010      /* limits for mprotect() etc */
-  ##define VM_MAYWRITE     0x00000020
-  ##define VM_MAYEXEC      0x00000040
-  ##define VM_MAYSHARE     0x00000080
+
+import sys
+
 mymap = { "VM_NONE" : 0, "VM_READ" : 0x00000001,
          "VM_WRITE" : 0x00000002, "VM_EXEC" : 0x00000004,
          "VM_SHARED": 0x00000008, "VM_MAYREAD": 0x00000010,
          "VM_MAYWRITE": 0x00000020, "VM_MAYEXEC": 0x00000040,
-         "VM_MAYSHARE": 0x00000080, "VM_ACCOUNT": 0x00100000 }
-foo = input()
-myinput = int(foo, base=16)
-#
+         "VM_MAYSHARE": 0x00000080, "VM_ACCOUNT": 0x00100000,
+         "VM_NORESERVE": 0x00200000, "VM_HUGETLB": 0x00400000}
+
+
+
+if len(sys.argv) < 2:
+    print(f"pass the value to convert: {sys.argv[0]} <vmflag in hex> ")
+    exit(1)
+
+myinput = int(sys.argv[1], base=16)
+
 for k, v in mymap.items():
     if v & myinput:
         myinput = ((~v) & myinput);
         print(k)
-print(hex(myinput)) # needs to zero
+
+if myinput:
+    print(f"Not all bits decoded {myinput}")
