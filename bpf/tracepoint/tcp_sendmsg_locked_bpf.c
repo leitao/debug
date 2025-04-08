3e164f59e8d8db9e5c0038b590a250a777444909
Breno Leitao <leitao@debian.org>
2025-04-08 11:02:00 -0700

    fixes

--- bpf/tracepoint/tcp_sendmsg_locked_bpf.c
+++ bpf/tracepoint/tcp_sendmsg_locked_bpf.c
@@ -1,29 +1,14 @@
-#include <linux/bpf.h>
+#include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 
 
-struct tcp_sendmsg_locked_args {
-    long __unused__;  // First 8 bytes are padding
-    const void *skb_addr;
-    int skb_len;
-    int msg_left;
-    int size_goal;
-};
-
-
-SEC("tracepoint/tcp/tcp_sendmsg_locked")
-int bpf_tcp_sendmsg_locked(struct tcp_sendmsg_locked_args *ctx)
+SEC("tp_btf/tcp_sendmsg_locked")
+int BPF_PROG(tcp_sendmsg_locked, struct sock *sk, struct msghdr *msg, struct sk_buff *skb, int size_goal)
 {
-    // Log to trace pipe for debugging
-    bpf_printk("TCP: skb_addr %p skb_len %d msg_left %d size_goal %d",
-               ctx->skb_addr, ctx->skb_len, ctx->msg_left, ctx->size_goal);
-    
-    /* bpf_printk("deref %d\n", *(int *) ctx->skb_addr); */
-    return 0;
+	bpf_printk("skb->len %d\n", skb->len);
+
+	return 0;
 }
-
-
-
 
 char LICENSE[] SEC("license") = "GPL";
