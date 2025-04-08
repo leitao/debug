4e1bde99e841166c63ee36deaa42ea938b1c656a
Breno Leitao <leitao@debian.org>
2025-04-08 07:15:48 -0700

    push

--- bpf/tracepoint/tcp_sendmsg_locked_bpf.c
+++ bpf/tracepoint/tcp_sendmsg_locked_bpf.c
@@ -11,37 +11,15 @@ struct tcp_sendmsg_locked_args {
     int size_goal;
 };
 
-struct event_data {
-    long pid;
-    long ts;
-    const void *skb_addr;
-    int skb_len;
-    int msg_left;
-    int size_goal;
-};
-
 
 SEC("tracepoint/tcp/tcp_sendmsg_locked")
 int bpf_tcp_sendmsg_locked(struct tcp_sendmsg_locked_args *ctx)
 {
-    struct event_data data = {};
-    
-    // Get process ID
-    data.pid = bpf_get_current_pid_tgid() >> 32;
-    
-    // Get timestamp
-    data.ts = bpf_ktime_get_ns();
-    
-    // Copy tracepoint data
-    data.skb_addr = ctx->skb_addr;
-    data.skb_len = ctx->skb_len;
-    data.msg_left = ctx->msg_left;
-    data.size_goal = ctx->size_goal;
-    
     // Log to trace pipe for debugging
     bpf_printk("TCP: skb_addr %p skb_len %d msg_left %d size_goal %d",
-               data.skb_addr, data.skb_len, data.msg_left, data.size_goal);
+               ctx->skb_addr, ctx->skb_len, ctx->msg_left, ctx->size_goal);
     
+    /* bpf_printk("deref %d\n", *(int *) ctx->skb_addr); */
     return 0;
 }
 
