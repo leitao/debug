0c2a553e5cd00e26ef456cd4f451603ec54459cc
Breno Leitao <leit@fb.com>
2025-05-23 08:17:25 -0700

    move changes

--- cgroups/cgoom/create_threads_and_malloc.c
+++ cgroups/cgoom/create_threads_and_malloc.c
@@ -4,17 +4,29 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define ALLOC_SIZE 1024*100
+#define ALLOC_SIZE 1024*1024*10 // 10 MB
+
 #define SLEEP_TIME 15
 
 int main() {
 	char *ptr;
 
-	// Sleep to wait for the other processes to start
+	// Sleep to wait for the other processes to be created
 	sleep(SLEEP_TIME);
 
 	ptr = malloc(ALLOC_SIZE);
-	*(char *)ptr = 'x';
+	for (int i = 0; i < ALLOC_SIZE; i++)
+		ptr[i] = 1;
+
+	sleep(1);
+
+
+	// Just it case the above didn't hit the CGMEM limits
+	//  Don't worry about the leaked memory
+	while (1) {
+		ptr = malloc(ALLOC_SIZE);
+		sleep(1);
+	};
 
 	return EXIT_SUCCESS;
 }
--- cgroups/cgoom/run.sh
+++ cgroups/cgoom/run.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
 
-sudo rmdir /sys/fs/cgroup/my_memcg || true
+# sudo rmdir /sys/fs/cgroup/my_memcg || true
 sudo mkdir /sys/fs/cgroup/my_memcg
 
 sudo dmesg -c > /dev/null
 
-echo "500M" | sudo tee /sys/fs/cgroup/my_memcg/memory.max
+echo "300M" | sudo tee /sys/fs/cgroup/my_memcg/memory.max
+echo "300M" | sudo tee /sys/fs/cgroup/my_memcg/memory.high
+
+cat /sys/fs/cgroup/my_memcg/memory.max
 
 # we want to kill all the processes in the cgroup
 # Without this, the problem doesn't happen
 echo 1 | sudo tee /sys/fs/cgroup/my_memcg/memory.oom.group
+echo 0 | sudo tee /sys/fs/cgroup/my_memcg/memory.swap.max
+
+cat /sys/fs/cgroup/my_memcg/memory.oom.group
 
 make
 echo "Starting the programs under my_memcg..."
-for i in $(seq 20000)
+for i in $(seq 5000)
 do
-	$(make run &) > /dev/null
+	make run &
 done
 # Wait here
 make run
-
-sudo rm -fr /sys/fs/cgroup/my_memcg
