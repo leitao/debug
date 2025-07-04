80447f6bdf20c8ea49285b2bd41acd457c7a11f6
Breno Leitao <leitao@debian.org>
2025-07-04 08:24:56 -0700

    improvments

--- bin/decode_bin.py
+++ bin/decode_bin.py
@@ -2,7 +2,6 @@
 
 import argparse
 
-
 def find_msb(n):
     """
     Using bit_length() - Most Pythonic and efficient
@@ -15,20 +14,27 @@ def isset(num, i):
     bit = 1 << i
     return bool(num & bit)
 
-def print_header(msb):
+def name(idx, description):
+    try:
+        return description[idx]
+    except IndexError:
+        return idx
+
+def print_header(msb, description):
     for i in range(msb, -1, -1):
-        print("{: >4}".format(i), end="")
+        p = name(i, description)
+        print("{: >6}".format(p), end="")
         if not i % 4:
-            print("  ", end="")
+            print(" ", end="")
     print()
-    print("="*msb*6)
+    print("="*msb*8)
 
 def print_num(num, msb):
     for i in range(msb, -1, -1):
         s = isset(num, i)
-        print("{: >4}".format(s), end="")
+        print("{: >6}".format(s), end="")
         if not i % 4:
-            print("  ", end="")
+            print(" ", end="")
     print(f"  ({num}) ({hex(num)})")
 
 
@@ -36,11 +42,16 @@ def print_num(num, msb):
 ## start here
 parser = argparse.ArgumentParser()
 parser.add_argument('number', nargs="+")           # positional argument
+parser.add_argument('--desc')
 args = parser.parse_args()
 nums = [int(i) for i in args.number]
 
+description = []
+if args.desc:
+    description = args.desc.split(',')
+
 msb = find_msb(max(nums))
-print_header(msb)
+print_header(msb, description)
 
 for num in nums:
     print_num(num, msb)
