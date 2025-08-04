81026c3d0c3ed233f5d037733a0a0666a6112308
Breno Leitao <leitao@debian.org>
2025-08-04 03:42:10 -0700

    fixes

--- arm/cpu_clock_frequency.c
+++ arm/cpu_clock_frequency.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <inttypes.h>
 
 #define M 1000000
 
-/* Example to check the frequency and precision of the clock */
-
 int main() {
     uint64_t cntfrq_el0;
-    double precision, freq_in_mhz;
+    double freq_in_mhz, precision_ns;
 
     asm volatile("mrs %0, cntfrq_el0" : "=r" (cntfrq_el0));
 
-    freq_in_mhz = cntfrq_el0 / M;
-    precision = 1/freq_in_mhz;
-    printf("cntfrq_el0: %lu\n", cntfrq_el0);
-    printf("frequency: %.1f MHz\n", freq_in_mhz);
-    printf("precision: %.3f (ns)\n", precision);
+    freq_in_mhz = (double)cntfrq_el0 / M;
+    precision_ns = 1000.0 / freq_in_mhz;  // ns per tick
+
+    printf("cntfrq_el0: %" PRIu64 "\n", cntfrq_el0);
+    printf("Frequency: %.1f MHz\n", freq_in_mhz);
+    printf("Precision: %.3f ns\n", precision_ns);
 
     return 0;
 }
